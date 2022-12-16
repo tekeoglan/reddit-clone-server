9bee7c4ee120a148ca5ce8d091478c127adb7086
tekeoglan <tekeoglan@DESKTOP-M4NM6QD>
2022-12-16 16:54:48 +0300

    feat(prisma): integrate prisma

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 
   if (module.hot) {
