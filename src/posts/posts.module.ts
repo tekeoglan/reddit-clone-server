f02cefc67eb6f60662b2d8b4360f43489733ee70
tekeoglan <tekeoglan@DESKTOP-M4NM6QD>
2022-12-25 12:47:00 +0300

    fix(): change prisma.service relative path to absolute path for testing

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 import { PostsController } from './posts.controller';
 
 @Module({
