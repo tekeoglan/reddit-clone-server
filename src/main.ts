9ef283c917c8ef36adcd76419a8966699aec8e8e
tekeoglan <tekeoglan@DESKTOP-M4NM6QD>
2023-01-26 17:52:57 +0300

    feat(): add cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,13 @@ import { Client } from 'pg';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['http://localhost:5173'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+  });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
